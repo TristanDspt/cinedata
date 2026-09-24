7b4caaacc1a38440e10b6805eaca081c7ee9b04a
TristanDspt <tristan.despont@hotmail.fr>
2026-09-24 11:52:04 +0200

    chore : safe_get deplacé et refacto dans utils.py

--- src/extract_api.py
+++ src/extract_api.py
@@ -7,6 +7,8 @@ import requests
 import time
 import yaml
 
+# --------------------------------------------------------------------------------
+
 load_dotenv()
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
@@ -32,40 +34,6 @@ def get_headers():
         "accept": "application/json",
         "Authorization": f"Bearer {API_KEY}"
     }
-
-
-def safe_get(url, retries=3):
-    """
-    Effectue une requête GET sécurisée avec gestion des erreurs et retry sur 429.
-
-    Args:
-        url (str): URL de la requête.
-        retries (int): Nombre de tentatives restantes en cas de 429. Défaut : 3.
-
-    Returns:
-        Response: Objet response requests, ou None en cas d'erreur.
-    """
-    if retries == 0:
-        print("Too much tentatives, try again later")
-        return None
-    try:
-        response = requests.get(url, headers=get_headers(), timeout=TIMEOUT)
-        response.raise_for_status()
-        return response
-    except requests.exceptions.ConnectionError:
-        print("Connexion error.")
-    except requests.exceptions.Timeout:
-        print("Timeout.")
-    except requests.exceptions.HTTPError as err:
-        if err.response.status_code == 429:
-            time_sleep = int(err.response.headers["Retry-After"]) + 1
-            time.sleep(time_sleep)
-            return safe_get(url, retries - 1)
-        else:
-            print(f"HTTP error : {err}")
-    except requests.exceptions.RequestException as err:
-        print(f"Unknown error : {err}")
-    return None
 
 
 def get_top_movie(limit=5):
